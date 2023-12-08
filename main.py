439c04668bbe645f1b388f0935cdb5a07d5afabe
mkmenta <mkmenta95@gmail.com>
2023-12-08 11:54:28 +0100

    Working with files

--- main.py
+++ main.py
@@ -7,14 +7,40 @@ from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.vectorstores import FAISS
 from dotenv import load_dotenv
+from langchain.document_loaders import TextLoader, PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+
+
+import os
 
 # load dotenv
 load_dotenv()
 
 
-vectorstore = FAISS.from_texts(
-    ["harrison worked at woowoo"], embedding=OpenAIEmbeddings()
-)
+def is_valid_document(file_path):
+    extensions = ('.pdf', '.txt')
+    return file_path.lower().endswith(extensions)
+
+
+all_files = [fn for fn in os.listdir('data') if is_valid_document(fn)]
+docs = []
+for file in all_files:
+    if file.endswith('.pdf'):
+        loader = PyPDFLoader
+    elif file.endswith('.txt'):
+        loader = TextLoader
+    else:
+        raise ValueError(f'Unknown file type: {file}')
+    loader = loader(os.path.join("data", file))
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    docs.extend(text_splitter.split_documents(documents))
+
+# vectorstore = FAISS.from_texts(
+#     ["harrison worked at woowoo"], embedding=OpenAIEmbeddings()
+# )
+vectorstore = FAISS.from_documents(docs, embedding=OpenAIEmbeddings())
+
 retriever = vectorstore.as_retriever()
 
 template = """Answer the question based only on the following context:
@@ -33,5 +59,5 @@ chain = (
     | StrOutputParser()
 )
 
-x = chain.invoke("where did harrison work?")
+x = chain.invoke("where did robertson work?")
 print(x)
