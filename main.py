90b120adcd4e931adefeac10b047504b9b20de18
mkmenta <mkmenta95@gmail.com>
2023-12-08 15:58:38 +0100

    Add incremental DB

--- main.py
+++ main.py
@@ -22,9 +22,17 @@ def is_valid_document(file_path):
     return file_path.lower().endswith(extensions)
 
 
+if os.path.exists("data/faiss_index"):
+    vectorstore = FAISS.load_local("data/faiss_index", embeddings=OpenAIEmbeddings())
+tracked_files = set()
+if os.path.exists('data/tracked_files'):
+    with open('data/tracked_files', 'r') as f:
+        for line in f:
+            tracked_files.add(line.strip())
 all_files = [fn for fn in os.listdir('data') if is_valid_document(fn)]
-docs = []
-for file in all_files:
+missing_files = [fn for fn in all_files if fn not in tracked_files]
+missing_docs = []
+for file in missing_files:
     if file.endswith('.pdf'):
         loader = PyPDFLoader
     elif file.endswith('.txt'):
@@ -34,12 +42,19 @@ for file in all_files:
     loader = loader(os.path.join("data", file))
     documents = loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    docs.extend(text_splitter.split_documents(documents))
+    missing_docs.extend(text_splitter.split_documents(documents))
 
 # vectorstore = FAISS.from_texts(
 #     ["harrison worked at woowoo"], embedding=OpenAIEmbeddings()
 # )
-vectorstore = FAISS.from_documents(docs, embedding=OpenAIEmbeddings())
+if missing_docs:
+    new_vectorstore = FAISS.from_documents(missing_docs, embedding=OpenAIEmbeddings())
+    vectorstore.merge_from(new_vectorstore)
+    vectorstore.save_local("data/faiss_index")
+
+    with open('data/tracked_files', 'w') as f:
+        for fn in all_files:
+            f.write(fn + '\n')
 
 retriever = vectorstore.as_retriever()
 
@@ -59,5 +74,5 @@ chain = (
     | StrOutputParser()
 )
 
-x = chain.invoke("where did robertson work?")
+x = chain.invoke("where did melbourne work?")
 print(x)
